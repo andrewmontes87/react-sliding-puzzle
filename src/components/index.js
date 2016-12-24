80415b1187f9500f80786185b1cbe91148d92076
andrewmontes87 <andrewmontes87@gmail.com>
2016-12-24 08:04:23 -0800

    cleaned out stuff not being used from starter project

--- src/components/index.js
+++ src/components/index.js
@@ -5,10 +5,4 @@
  *
  */
 
-export CounterButton from './CounterButton/CounterButton';
-export GithubButton from './GithubButton/GithubButton';
-export InfoBar from './InfoBar/InfoBar';
 export PuzzleBox from './PuzzleBox/PuzzleBox';
-export MiniInfoBar from './MiniInfoBar/MiniInfoBar';
-export SurveyForm from './SurveyForm/SurveyForm';
-export WidgetForm from './WidgetForm/WidgetForm';
