b187ac24dcebad9db2ae6c8989b2e88e33cbbf69
andrewmontes87 <andrewmontes87@gmail.com>
2016-12-24 00:22:14 -0800

    some cleanup and commenting but still some bugs with moveElement()

--- src/components/PuzzleBox/PuzzleBox.js
+++ src/components/PuzzleBox/PuzzleBox.js
@@ -16,58 +16,34 @@ export default class PuzzleBox extends Component {
     };
   }
 
+  // ON MOUNT, SHUFFLE THE BOARD
   componentDidMount() {
     this.shufflePuzzle(this.state.dim);
   }
 
+  //
+  // PUZZLE FUNCTIONS
+  //
+
+  // CREATE A NEW SOLVED BOARD
   prepData(dim) {
     const data = [];
-    // for each row...
     for (let idx = 0; idx < dim; idx++) {
-      // push a new empty array
       data.push([]);
-      // for each cell...
       for (let nidx = 1 + (idx * dim); nidx <= dim + (idx * dim); nidx++) {
-        // push a new number
-        if (nidx) {
-          data[idx].push(nidx);
-        }
+        data[idx].push(nidx);
       }
     }
     data[dim - 1][dim - 1] = 0;
     return data;
   }
 
-  shufflePuzzle(dim) {
-    const minShuffle = dim;
-    const maxShuffle = dim * 2;
-    const steps = Math.floor(Math.random() * (maxShuffle - minShuffle + 1)) + minShuffle;
-    const solutionMatrix = this.prepData(dim);
-    let puzzleMatrix = this.prepData(dim);
-    this.state.lastMoves = [];
-    for (let vg = 0; vg < steps; vg++) {
-      const next = this.findNext(puzzleMatrix);
-      puzzleMatrix = this.moveElement(puzzleMatrix, next.row, next.cell);
-    }
-    this.setState({
-      dim: dim,
-      puzzleMatrix: puzzleMatrix,
-      solutionMatrix: solutionMatrix
-    });
+  // CHECK IF THE BOARD IS SOLVED
+  checkIfSolved(puzzleMatrix, solutionMatrix) {
+    return JSON.stringify(puzzleMatrix) === JSON.stringify(solutionMatrix);
   }
 
-  resetPuzzle(dim) {
-    const puzzleMatrix = this.prepData(dim);
-    const solutionMatrix = this.prepData(dim);
-    this.setState({
-      dim: dim,
-      puzzleMatrix: puzzleMatrix,
-      solutionMatrix: solutionMatrix,
-      lastMoves: [],
-      isSolved: true
-    });
-  }
-
+  // FIND THE POSITION OF THE EMPTY SPACE
   findEmpty(puzzleMatrix) {
     const dim = puzzleMatrix.length;
     for (let ir = 0; ir < dim; ir++) {
@@ -83,87 +59,105 @@ export default class PuzzleBox extends Component {
     return null;
   }
 
+  // FIND A RANDOM NEXT PIECE TO SLIDE TO THE EMPTY SPACE
   findNext(puzzleMatrix) {
     const dim = puzzleMatrix.length;
+    const lastMove = this.state.lastMoves[0];
+    const possibleMoves = [];
+
+    // find the row, column indexes of the empty space
     const empty = this.findEmpty(puzzleMatrix);
     const ri = empty.row;
     const ci = empty.cell;
-    const nextOptions = [];
+
+    // row, column indexes of next moves in four direcionts
     const topri = ri - 1 >= 0 ? ri - 1 : null;
     const topci = ci;
+    const rightri = ri;
+    const rightci = ci + 1 < dim ? ci + 1 : null;
+    const bottomri = ri + 1 < dim ? ri + 1 : null;
+    const bottomci = ci;
+    const leftri = ri;
+    const leftci = ci - 1 >= 0 ? ci - 1 : null;
+
+    // check four directions for possible moves
     if (topri !== null && topci !== null) {
-      nextOptions.push({
+      possibleMoves.push({
         row: topri,
         cell: topci
       });
     }
-    const rightri = ri;
-    const rightci = ci + 1 < dim ? ci + 1 : null;
     if (rightri !== null && rightci !== null) {
-      nextOptions.push({
+      possibleMoves.push({
         row: rightri,
         cell: rightci
       });
     }
-    const bottomri = ri + 1 < dim ? ri + 1 : null;
-    const bottomci = ci;
     if (bottomri !== null && bottomci !== null) {
-      nextOptions.push({
+      possibleMoves.push({
         row: bottomri,
         cell: bottomci
       });
     }
-    const leftri = ri;
-    const leftci = ci - 1 >= 0 ? ci - 1 : null;
     if (leftri !== null && leftci !== null) {
-      nextOptions.push({
+      possibleMoves.push({
         row: leftri,
         cell: leftci
       });
     }
-    let randOption = nextOptions[Math.floor(Math.random() * nextOptions.length)];
-    while (puzzleMatrix[randOption.row][randOption.cell] === this.state.lastMoves[0]) {
-      randOption = nextOptions[Math.floor(Math.random() * nextOptions.length)];
+
+    // choose a random move
+    let randomMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+    // don't repeat the last move
+    while (puzzleMatrix[randomMove.row][randomMove.cell] === lastMove) {
+      randomMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
     }
-    return randOption;
+
+    return randomMove;
   }
 
+  // MOVE AN ELEMENT TO THE EMPTY SPACE IN A PUZZLE MATRIX
   moveElement(puzzleMatrix, row, cell) {
     const dim = puzzleMatrix.length;
-    const refCell = puzzleMatrix[row][cell];
+    const movingElement = puzzleMatrix[row][cell];
+
     // if already the empty cell, return untouched puzzleMatrix
     if (puzzleMatrix[row][cell] === 0) return puzzleMatrix;
+
     // if not, check available nearby cells
+    // first set limits based on where the piece is on the board
     const top = row > 0 ? puzzleMatrix[row - 1][cell] : -1;
     const right = cell < dim - 1 ? puzzleMatrix[row][cell + 1] : -1;
     const bottom = row < dim - 1 ? puzzleMatrix[row + 1][cell] : -1;
     const left = cell > 0 ? puzzleMatrix[row][cell - 1] : -1;
+
     // if any of the nearby cells are the empty, swap places
     let tmp;
     if (!top) {
       tmp = puzzleMatrix[row - 1][cell];
-      puzzleMatrix[row - 1][cell] = refCell;
+      puzzleMatrix[row - 1][cell] = movingElement;
     } else if (!right) {
       tmp = puzzleMatrix[row][cell + 1];
-      puzzleMatrix[row][cell + 1] = refCell;
+      puzzleMatrix[row][cell + 1] = movingElement;
     } else if (!bottom) {
       tmp = puzzleMatrix[row + 1][cell];
-      puzzleMatrix[row + 1][cell] = refCell;
+      puzzleMatrix[row + 1][cell] = movingElement;
     } else if (!left) {
       tmp = puzzleMatrix[row][cell - 1];
-      puzzleMatrix[row][cell - 1] = refCell;
+      puzzleMatrix[row][cell - 1] = movingElement;
     }
+
     puzzleMatrix[row][cell] = tmp;
-    this.state.lastMoves.unshift(refCell);
-    this.checkIfSolved(puzzleMatrix);
+    this.state.lastMoves.unshift(movingElement);
+
     return puzzleMatrix;
   }
 
-  checkIfSolved(puzzleMatrix) {
-    this.setState({ isSolved: JSON.stringify(puzzleMatrix) === JSON.stringify(this.state.solutionMatrix) });
-  }
+  //
+  // UI HANDLERS
+  //
 
-
+  // DROPDOWN FORM HANDLERS
   handleNChange(event) {
     const dim = parseInt(event.target.value, 10);
     this.shufflePuzzle(dim);
@@ -173,13 +167,59 @@ export default class PuzzleBox extends Component {
     event.preventDefault();
   }
 
+  //
+  // STATE SETTER UI HANDLERS
+  //
+
+  // BUILD A SOLVED BOARD AND SET STATE
+  resetPuzzle(dim) {
+    const puzzleMatrix = this.prepData(dim);
+    const solutionMatrix = this.prepData(dim);
+    this.setState({
+      dim: dim,
+      puzzleMatrix: puzzleMatrix,
+      solutionMatrix: solutionMatrix,
+      lastMoves: [],
+      isSolved: true
+    });
+  }
+
+  // BUILD A SHUFFLED BOARD AND SET STATE
+  shufflePuzzle(dim) {
+    const minShuffle = dim;
+    const maxShuffle = dim * 2;
+    const steps = Math.floor(Math.random() * (maxShuffle - minShuffle + 1)) + minShuffle;
+    const solutionMatrix = this.prepData(dim);
+    let puzzleMatrix = this.prepData(dim);
+    this.state.lastMoves = [];
+    let solved;
+    for (let vg = 0; vg < steps; vg++) {
+      const next = this.findNext(puzzleMatrix);
+      puzzleMatrix = this.moveElement(puzzleMatrix, next.row, next.cell);
+      solved = this.checkIfSolved(puzzleMatrix, this.state.solutionMatrix);
+    }
+    this.setState({
+      dim: dim,
+      puzzleMatrix: puzzleMatrix,
+      solutionMatrix: solutionMatrix,
+      isSolved: solved
+    });
+  }
+
   render() {
     const styles = require('./PuzzleBox.scss');
+
+    // UI HANDLER TO MOVE A CLICKED TILE AND SET STATE
     const moveElementHandler = (ridx, cidx) => {
       let puzzleMatrix = this.state.puzzleMatrix;
       puzzleMatrix = this.moveElement(puzzleMatrix, ridx, cidx);
-      this.setState({ puzzleMatrix: puzzleMatrix });
+      const solved = this.checkIfSolved(puzzleMatrix, this.state.solutionMatrix);
+      this.setState({
+        puzzleMatrix: puzzleMatrix,
+        isSolved: solved
+      });
     };
+
     return (<div>
         <form onSubmit={this.handleSubmit.bind(this)}>
           <label>
